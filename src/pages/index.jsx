f854007e908f224660be0fd6cedeab3d7e2e0945
connorbrett <connorbrett@email.arizona.edu>
2019-05-04 20:45:11 -0700

    undo test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -208,8 +208,7 @@ const Index = () => (
               1,000+ attendees <br/>
               Jan. 19-21, 2020<br/>
               University of Arizona <br/>
-              Location TBD <br/>
-              Test
+              Location TBD
             </Subtitle>
             <RegistrationCTA target="_blank" href={RegLink}>
               Apply now!
