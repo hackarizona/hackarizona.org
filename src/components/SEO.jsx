33a01790ad2b07e532520d15c9ac38942c6c3371
Andrew <andrewjmrls@gmail.com>
2019-05-08 10:50:15 -0700

    Updated site description

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -45,18 +45,18 @@ const SEO = () => {
     <Helmet>
       <html lang={config.siteLanguage} />
       <title>Hack Arizona</title>
-      <meta name="description" content='Hack Arizona is the largest hackathon in the Southwest. January 18-20, 2019. Learn more and register now!' />
+      <meta name="description" content='Hack Arizona is the largest hackathon in the Southwest. January 17-19, 2020. Learn more and register now!' />
       <link rel="icon" type="image/png" sizes="32x32" href={Favicon} />
       <link rel="icon" type="image/png" sizes='16x16' href={Favicon} />
       <meta property="og:locale" content={config.ogLanguage} />
       <meta property="og:site_name" content='Hack Arizona' />
       <meta property="og:image" content={MetaImage} />
       <meta property="og:title" content="Hack Arizona | The largest hackathon in the Southwest"></meta>
-      <meta property="og:description" content='Hack Arizona is the largest hackathon in the Southwest. January 18-20, 2019. Learn more and register now!' />
+      <meta property="og:description" content='Hack Arizona is the largest hackathon in the Southwest. January 17-19, 2020. Learn more and register now!' />
       <meta property="og:image" content={MetaImage}  />
       <meta property="og:url" content="https://hackarizona.org" />
       <meta name="twitter:title" content="Hack Arizona | The largest hackathon in the Southwest "></meta>
-      <meta name="twitter:description" content='Hack Arizona is the largest hackathon in the Southwest. January 18-20, 2019. Learn more and register now!'/>
+      <meta name="twitter:description" content='Hack Arizona is the largest hackathon in the Southwest. January 17-19, 2020. Learn more and register now!'/>
       <meta name="twitter:image" content={MetaImage} />
       <meta name="twitter:card" content={MetaImage}  />
       <div _dangerouslySetInnerHTML={{__html: BuildFromHTML()}}/>
